cbad104bf30b5ce065e2264f8a580e4e5b412421
D-A-W-N <ichweisswasdugetanhastxd@googlemail.com>
2016-01-09 00:12:26 +0100

    Initial commit.

--- IrcClient/IrcClient/IrcClient.cs
+++ IrcClient/IrcClient/IrcClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.IO;
@@ -12,6 +13,30 @@ using System.Xml;
 
 namespace GangwarsBot
 {
+	class Colors
+	{
+		public static char NORMAL = (char)15;
+		public static char BOLD = (char)2;
+		public static char UNDERLINE = (char)31;
+		public static char REVERSE = (char)22;
+		public static string WHITE = (char)3 + "00,1";
+		public static string BLACK = (char)3 + "01,0";
+		public static string DARK_BLUE = (char)3 + "02";
+		public static string DARK_GREEN = (char)3 + "03";
+		public static string RED = (char)3 + "04";
+		public static string BROWN = (char)3 + "05";
+		public static string PURPLE = (char)3 + "06";
+		public static string OLIVE = (char)3 + "07";
+		public static string YELLOW = (char)3 + "08";
+		public static string GREEN = (char)3 + "09";
+		public static string TEAL = (char)3 + "10";
+		public static string CYAN = (char)3 + "11";
+		public static string BLUE = (char)3 + "12";
+		public static string MAGENTA = (char)3 + "13";
+		public static string DARK_GRAY = (char)3 + "14";
+		public static string LIGHT_GRAY = (char)3 + "15";
+	}
+
 	public class IrcClient
 	{
 		public string Server{ get; set; }
@@ -200,7 +225,7 @@ namespace GangwarsBot
 				if (Channel == null) {
 					Channel = DefaultChannel;
 				}
-				OutLine = "PRIVMSG " + Channel + " :" + Output;
+				OutLine = "PRIVMSG " + Channel + " :" + Colors.BLACK + " " + Output + " ";
 			}
 			Console.ForegroundColor = ConsoleColor.Red;
 			Console.WriteLine ("OUT: " + OutLine);
