edad0e27bb6ee290fe678b5cfa7d8f45aad4dcdf
D-A-W-N <ichweisswasdugetanhastxd@googlemail.com>
2016-01-08 22:20:49 +0100

    Initial commit.

--- IrcClient/IrcClient/IrcClient.cs
+++ IrcClient/IrcClient/IrcClient.cs
@@ -1,27 +1,53 @@
 ﻿using System;
+using System.Data;
+using System.Data.Common;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
 
 namespace GangwarsBot
 {
 	public class IrcClient
 	{
-		private string Server{ get; set; }
+		public string Server{ get; set; }
 
-		private int Port{ get; set; }
+		public int Port{ get; set; }
 
-		private string User{ get; set; }
+		public string User{ get; set; }
 
-		private string Nick{ get; set; }
+		public string Nick{ get; set; }
 
-		private string DefaultChannel{ get; set; }
+		public string DefaultChannel{ get; set; }
 
 		private TcpClient Client;
-		private NetworkStream Stream;
 		private StreamReader Reader;
 		private StreamWriter Writer;
+
+		private DataTable getHostEntrys (string Path)
+		{
+			DataTable HostTable = new DataTable ("HostEntrys");
+
+			if (File.Exists (Path)) {
+				HostTable.ReadXml (Path);
+			} else {
+				// Datei existiert noch nicht - Erstellen der Spalten
+				HostTable.Columns.Add ("Host", typeof(string));
+
+				//Anlegen der default-Werte
+				DataRow row = HostTable.NewRow ();
+				row ["Host"] = "Dawn.quakenet.org";
+
+				HostTable.Rows.Add (row);
+
+				HostTable.WriteXml (Path, XmlWriteMode.WriteSchema);
+			}
+
+			return HostTable;
+		}
 
 
 		public IrcClient ()
@@ -30,18 +56,24 @@ namespace GangwarsBot
 
 		public void Connect ()
 		{
-			Client = new TcpClient ();
-			Client.Connect (Server, Port);
-			if (Client.Connected) {
-				Stream = Client.GetStream ();
-				Reader = new StreamReader (Stream);
-				Writer = new StreamWriter (Stream);
+			try {
+				Client = new TcpClient ();
+				Client.Connect (Server, Port);
+				Reader = new StreamReader (Client.GetStream ());
+				Writer = new StreamWriter (Client.GetStream ());
+				SendAuth ();
+				ReadResponse ();
+
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
 			}
 		}
 
 		public void Dispose ()
 		{
-			
+			Writer.Dispose ();
+			Reader.Dispose ();
+			Client.Client.Dispose ();
 		}
 
 		private void SendAuth ()
@@ -55,6 +87,21 @@ namespace GangwarsBot
 			return true;
 		}
 
+		private void JoinChannel (string Channel = null, string Key = null)
+		{
+			string OutLine;
+			if (Channel == null) {
+				OutLine = "JOIN " + DefaultChannel;
+			} else {
+				if (Key == null) {
+					OutLine = "JOIN " + Channel;
+				} else {
+					OutLine = "JOIN " + Channel + " " + Key;
+				}
+			}
+			SendResponse (OutLine, true);
+		}
+
 		private void ParseChannelCommand (string ChannelCommand, string Channel)
 		{
 			
@@ -62,27 +109,39 @@ namespace GangwarsBot
 
 		private void ReadResponse ()
 		{
-			string ReadLine = null;
+			string ReadedLine;
 			while (true) {
-				while ((ReadLine = Reader.ReadLine ()) != null) {
-					Console.WriteLine ("IN: " + ReadLine);
-					string[] LineSplit = ReadLine.Split (new Char[] { ' ' });
+				while ((ReadedLine = Reader.ReadLine ()) != null) {
+					Console.WriteLine ("IN: " + ReadedLine);
+					string[] LineSplit = ReadedLine.Split (new Char[] { ' ' });
 
 					if (LineSplit [0] == "PING") {
 						SendResponse ("PONG " + LineSplit [1], true);
 					}
 
 					switch (LineSplit [1]) {
-					case "":
+					case "221":
+						JoinChannel ();
 						break;
 					}
 				}
 			}
 		}
 
-		private void SendResponse (string Output, bool Cmd = null, string Channel = null)
+		private void SendResponse (string Output, bool Cmd = false, string Channel = null)
 		{
-			
+			string OutLine;
+			if (Cmd) {
+				OutLine = Output;
+			} else {
+				if (Channel == null) {
+					Channel = DefaultChannel;
+				}
+				OutLine = "PRIVMSG " + Channel + " :" + Output;
+			}
+			Console.WriteLine ("OUT: " + OutLine);
+			Writer.WriteLine (OutLine);
+			Writer.Flush ();
 		}
 	}
 }
--- IrcClient/IrcClient/Program.cs
+++ IrcClient/IrcClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using GangwarsBot;
 
 namespace GangwarsBot
@@ -7,7 +9,19 @@ namespace GangwarsBot
 	{
 		public static void Main (string[] args)
 		{
-			
+			try {
+				IrcClient irc = new IrcClient ();
+				irc.Server = "azubu.jp.quakenet.org";
+				irc.Port = 6669;
+				irc.User = "PBot";
+				irc.Nick = "[D]P-Bot";
+				irc.DefaultChannel = "#test.news";
+
+				irc.Connect ();
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
+			}
+
 		}
 	}
 }
