f5eed780c1e88ac5275a0a3f9d8e96500412ce48
D-A-W-N <ichweisswasdugetanhastxd@googlemail.com>
2016-01-09 01:57:15 +0100

    Initial commit.

---
+++ ImapClient/ImapClient/ImapClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using S22.Imap;
+
+namespace GangwarsBot
+{
+	public class ImapListener
+	{
+		public string Server{ get; set; }
+
+		public int Port{ get; set; }
+
+		public string User{ get; set; }
+
+		public string Pass{ get; set; }
+
+		static AutoResetEvent reconnectEvent = new AutoResetEvent (false);
+		static ImapClient Client;
+
+		public ImapListener ()
+		{
+		}
+
+		public void Connect ()
+		{
+			try {
+				while (true) {
+					Console.Write ("Connecting...");
+					InitializeClient ();
+					Console.WriteLine ("OK");
+
+					reconnectEvent.WaitOne ();
+				}
+			} finally {
+				if (Client != null)
+					Client.Dispose ();
+			}
+		}
+
+		private void InitializeClient ()
+		{
+			// Dispose of existing instance, if any.
+			if (Client != null)
+				Client.Dispose ();
+			Client = new ImapClient (Server, Port, User, Pass, AuthMethod.Login);
+			// Setup event handlers.
+			Client.NewMessage += client_NewMessage;
+			Client.IdleError += client_IdleError;
+		}
+
+		static void client_IdleError (object sender, IdleErrorEventArgs e)
+		{
+			Console.Write ("An error occurred while idling: ");
+			Console.WriteLine (e.Exception.Message);
+
+			reconnectEvent.Set ();
+		}
+
+		static void client_NewMessage (object sender, IdleMessageEventArgs e)
+		{
+			Console.WriteLine ("Got a new message, uid = " + e.MessageUID);
+		}
+	}
+}
+
--- ImapClient/ImapClient/Program.cs
+++ ImapClient/ImapClient/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GangwarsBot;
 
-namespace ImapClient
+namespace GangwarsBot
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			try {
+				ImapListener imap = new ImapListener ();
+				imap.Server = "imap.1blu.de";
+				imap.Port = 143;
+				imap.User = "z233474_0-catchall";
+				imap.Pass = "catchall";
+
+				imap.Connect ();
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
+			}
+
 		}
 	}
 }
